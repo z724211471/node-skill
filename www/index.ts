7524833c0e9ba24baa89300514036b10cf01e048
724211471 <724211471@qq.com>
2019-12-14 18:18:25 +0800

    增加用户库

--- www/index.ts
+++ www/index.ts
@@ -1,7 +1,7 @@
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
 import * as bodyParser from 'koa-bodyparser'
-import connectDatabase from '../src/db/index'
+//import connectDatabase from '../src/db/index'
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { userReg, userCode } from '../src/user/index'
@@ -9,12 +9,28 @@ const app = new Koa()
 
 // const User = require('../db/user/user')
 let router = new Router()
-const dburl = 'mongodb://127.0.0.1:27017/mydb'
-connectDatabase(dburl)
+// const dburl = 'mongodb://127.0.0.1:27017/mydb'
+// connectDatabase(dburl)
 // app.use(async ctx => {
 //   ctx.body = 'Hello World'
 // })
+createConnection({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "czy",
+  password: "123456",
+  database: "test",
+  entities:['../src/db/*.ts'],
+  synchronize: true,
+  logging: false,
+}).then(rec=>{
+  console.log('成功',rec)
+}).catch(err=>{
+  console.log('失败',err)
+})
 app.use(bodyParser())
+
 router.post('/re', userReg)
 router.post('/code', userCode)
 app.use(router.routes()).use(router.allowedMethods())
